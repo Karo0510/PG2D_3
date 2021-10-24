4d1df612c02ca95c6cc6c562fab0bc7ddfb69d4e
Karo0510 <karma0510@gmail.com>
2021-10-24 10:30:18 +0200

    plansza - droga prosta + paski boczne

--- game.js
+++ game.js
@@ -19,18 +19,39 @@ function drawStraightRoad()
     ctx.stroke();
     ctx.closePath();
 
+  
+}
+
+function drawDashLine()
+{
     ctx.beginPath();
     ctx.setLineDash([10, 10]);
     ctx.moveTo(400, 0);
     ctx.lineTo(400, 600);
     ctx.strokeStyle = '#ffffff';
     ctx.stroke();
-}
-
-function drawStripes()
-{
-   
 } 
 
-drawStraightRoad();
-//drawStripes();
+function drawStripes(x)
+{
+    ctx.beginPath();
+    ctx.setLineDash([20,20]);
+    ctx.moveTo(x, 0);
+    ctx.lineTo(y, 600);
+    ctx.lineWidth = 20;
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+}
+
+function board()
+{
+    drawStraightRoad();
+    drawDashLine();
+    drawStripes(200);
+    drawStripes(600);
+}
+
+
+board();
+
+
