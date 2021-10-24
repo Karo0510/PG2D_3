4f3fcab829501b39605b386434720bed6238a7e8
Karo0510 <karma0510@gmail.com>
2021-10-24 10:13:44 +0200

    droga

--- game.js
+++ game.js
@@ -4,13 +4,33 @@ var ctx = c.getContext("2d");
 height = c.clientHeight;
 width = c.clientWidth;
 
-function drawStraight()
+
+ctx.rect(0,0,width,height);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.stroke();
+
+function drawStraightRoad()
 {
     ctx.beginPath();
-    ctx.rect(400, 0, 200, height);
+    ctx.rect(200, 0, 400, height);
     ctx.fillStyle = "black";
     ctx.fill();
     ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.setLineDash([10, 10]);
+    ctx.moveTo(400, 0);
+    ctx.lineTo(400, 600);
+    ctx.strokeStyle = '#ffffff';
+    ctx.stroke();
 }
 
-drawStraight();
+function drawStripes()
+{
+   
+} 
+
+drawStraightRoad();
+//drawStripes();
